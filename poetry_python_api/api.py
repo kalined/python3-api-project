70fcd510546998e0aa7c9d8e9ff0a78def4ee63d
Edgar Kalinovski <D000036@danskenet.net>
2024-02-27 15:07:37 +0200

    Add new event

--- poetry_python_api/api.py
+++ poetry_python_api/api.py
@@ -1,9 +1,42 @@
 from pathlib import Path
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from starlette.staticfiles import StaticFiles
+
+from poetry_python_api.database import SessionLocal
+from poetry_python_api.models import DBEvent
+from poetry_python_api.schemas import Event
 
 app = FastAPI()
 
+
 PROJECT_ROOT = Path(__file__).parent.parent
+
+
+def get_session() -> Session:
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+
+
+# Add an operation @app.get
+@app.get("/event/{id}", response_model=Event)
+def get_event(id: int, db: Annotated[Session, Depends(get_session)]) -> DBEvent:
+    event = db.get(DBEvent, id)
+    if event is None:
+        raise HTTPException(status_code=404, detail=f"No product with id {id}")
+    return event
+
+
+# Return all events in the database
+@app.get("/event", response_model=list[Event])
+def get_all_events(db: Annotated[Session, Depends(get_session)]) -> list[DBEvent]:
+    return db.execute(select(DBEvent)).scalars()
+
+
 app.mount("/", StaticFiles(directory=PROJECT_ROOT / "static", html=True))
